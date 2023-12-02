fe54d49faaa6ebf4d94ffd4238ccdecb9f65db92
ngchongin <ngchongin@gmail.com>
2023-12-02 15:47:20 -0500

    pass serial test between arduino and pyserial

--- game/game.ino
+++ game/game.ino
@@ -1,37 +1,32 @@
 #include <ArduinoJson.h>
-
-void setup() {
-  Serial.begin(9600);  // Set the baud rate to match the Python code
+StaticJsonDocument<400> doc;
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Ready");
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    // Read the JSON data from Python
-    StaticJsonDocument<200> doc;
-    DeserializationError error = deserializeJson(doc, Serial);
-
-    if (error) {
-      Serial.print("Error deserializing JSON: ");
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    delay(10);
+    String s = Serial.readStringUntil('\n');
+    
+    DeserializationError error = deserializeJson(doc, s);
+    if (error) 
+    {
+      Serial.print(F("deserializeJson() failed: "));
       Serial.println(error.c_str());
       return;
     }
-
-    // Extract data from JSON
-    const char* command = doc["command"];
-    int value = doc["value"];
-
-    // Process the command
-    if (strcmp(command, "LED") == 0) {
-      // Example: Control an LED based on the received value
-      digitalWrite(LED_BUILTIN, value);
-    }
-
-    // Send a response back to Python
+    
     doc.clear();
-    doc["response"] = "ACK";
-    serializeJson(doc, Serial);
-    Serial.println();
-
-    delay(500);  // Add a delay for stability
+    doc["command"] = "test";
+    doc["result"] = "OK";
+    doc["message"] = s;
+    String jsonString;
+    serializeJson(doc, jsonString);
+    Serial.println(jsonString);
   }
 }
--- webserver/arduino_comunication.py
+++ webserver/arduino_comunication.py
@@ -3,23 +3,35 @@ import time
 import json
 
 # Define the serial port and baud rate
-ser = serial.Serial('/dev/ttyACM0', 9600)  # Update the port accordingly
+raspberrypi_port = '/dev/ttyACM0'
+mac_port = '/dev/cu.usbmodem1401'
+ser = serial.Serial(mac_port, 115200)  # Update the port accordingly
 
 try:
+    count = 0
     while True:
         # Create a JSON object
+        count += 1
+        result = 1 if count % 2 == 0 else 1
         data = {
             "command": "LED",
             "value": 1  # Change this value based on your needs
         }
 
+        sd = json.dumps(data).encode('utf-8')
+        print(sd)
         # Send JSON data to Arduino
-        ser.write(json.dumps(data).encode('utf-8'))
+        ser.write(sd)
 
         # Read response from Arduino
         response = ser.readline().decode('utf-8')
         if response:
-            print(f"Raspberry Pi received response: {response}")
+            print(response)
+            if 'Ready' in response:
+                print(f"Receive Ready.#{response}")
+            else:
+                hash = json.loads(response)
+                print(f"Raspberry Pi received response: {hash}")
 
         time.sleep(1)  # Add a delay for stability
 
