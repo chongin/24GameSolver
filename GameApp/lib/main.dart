4b73278c7e957939c75845f0919e8bf9302cf1d1
ngchongin <ngchongin@gmail.com>
2023-12-03 16:15:40 -0500

    update ui

--- GameApp/lib/main.dart
+++ GameApp/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: '24 Game Solver',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(),
     );
   }
@@ -21,27 +23,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> digits = [];
-  List<String> symbols = ['(', ')', '+', '-', '*', '/'];
-
-  @override
-  void initState() {
-    super.initState();
-    // Mock API call for demo purposes
-    mockApiCall().then((List<int> result) {
-      setState(() {
-        digits = result;
-      });
-    });
-  }
-
-  Future<List<int>> mockApiCall() async {
-    // Simulating an API call delay
-    await Future.delayed(Duration(seconds: 2));
-
-    // Mock response from the server
-    return [2, 4, 6, 8]; // Replace with your actual API response
-  }
+  List<int> digits = [2, 4, 6, 8];
+  List<String> symbols = ['+', '-', '*', '/', '(', ')'];
+  List<List<String?>> selectedValues = List.generate(11, (_) => [null]);
 
   @override
   Widget build(BuildContext context) {
@@ -95,28 +79,62 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               SizedBox(width: 10),
-              for (String symbol in symbols)
+              for (int i = 0; i < 11; i++)
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        symbol,
-                        style: TextStyle(fontSize: 24, color: Colors.white),
-                      ),
-                    ),
+                  child: CustomDropdownButton(
+                    items: getDropdownItems(),
+                    value: selectedValues[i][0],
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedValues[i][0] = value;
+                      });
+                    },
                   ),
                 ),
             ],
           ),
         ],
       ),
+    );
+  }
+
+  List<DropdownMenuItem<String?>> getDropdownItems() {
+    List<String> items = [...symbols, ...digits.map((digit) => digit.toString())];
+
+    return items
+        .map(
+          (item) => DropdownMenuItem<String?>(
+        value: item,
+        child: Text(item),
+      ),
+    )
+        .toList();
+  }
+}
+
+class CustomDropdownButton<T> extends StatefulWidget {
+  final List<DropdownMenuItem<T>> items;
+  final T? value;
+  final ValueChanged<T?>? onChanged;
+
+  CustomDropdownButton({
+    required this.items,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  _CustomDropdownButtonState<T> createState() => _CustomDropdownButtonState<T>();
+}
+
+class _CustomDropdownButtonState<T> extends State<CustomDropdownButton<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<T>(
+      items: widget.items,
+      value: widget.value,
+      onChanged: widget.onChanged,
     );
   }
 }
