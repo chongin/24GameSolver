523f2f793e1ff3e69860e12398a7c7feea78b29f
ngchongin <ngchongin@gmail.com>
2023-12-05 13:00:50 -0500

    add camera view

--- GameApp/lib/main.dart
+++ GameApp/lib/main.dart
@@ -1,13 +1,20 @@
-import 'package:flutter/material.dart';
-import 'api_client.dart'; // Import the ApiClient
 import 'dart:async';
+import 'api_client.dart';
 import 'error_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
+
+  const MyApp({Key? key, required this.cameras}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,12 +22,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(cameras: cameras),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  MyHomePage({Key? key, required this.cameras}) : super(key: key);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -33,6 +44,20 @@ class _MyHomePageState extends State<MyHomePage> {
   late Timer _timer = Timer(Duration.zero, () {});
   int _timerSeconds = 120; // Initial value for the timer
   bool isGameStarted = false;
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  late CameraDescription _selectedCamera;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedCamera = widget.cameras.first;
+    _controller = CameraController(
+      _selectedCamera,
+      ResolutionPreset.medium,
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
 
   void startTimer() {
     stopTimer();
@@ -54,6 +79,13 @@ class _MyHomePageState extends State<MyHomePage> {
     _timer.cancel();
     _timer = Timer(Duration.zero, () {});
     ;
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<void> _showTimeoutDialog() async {
@@ -79,184 +111,210 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '24 Game Solver',
-          style: TextStyle(
-            fontSize: 30, // Adjust the font size as needed
-            fontWeight: FontWeight.bold, // Add bold font weight
-            color: Colors.deepPurple, // Set your desired color
+        appBar: AppBar(
+          title: Text(
+            '24 Game Solver',
+            style: TextStyle(
+              fontSize: 30, // Adjust the font size as needed
+              fontWeight: FontWeight.bold, // Add bold font weight
+              color: Colors.deepPurple, // Set your desired color
+            ),
           ),
         ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 20),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Container(
-              padding: EdgeInsets.all(60),
-              decoration: BoxDecoration(
-                color: Colors.blue, // Set your desired background color
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'Time Remain: $_timerSeconds seconds',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white, // Set your desired text color
-                ),
+        body: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Container(
+                      padding: EdgeInsets.all(60),
+                      decoration: BoxDecoration(
+                        color: Colors.blue, // Set your desired background color
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'Time Remain: $_timerSeconds seconds',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white, // Set your desired text color
+                        ),
+                      ),
+                    ),
+                  ]),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          bool success = await startNewGame();
+                          setState(() {
+                            isGameStarted = success;
+                          });
+                        },
+                        child: Text('New Game'),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: isGameStarted ? () async {
+                          await calculateAndUpdateResult();
+                          setState(() {
+                            isGameStarted = false;
+                          });
+                        } : null,
+                        child: Text('Calculate'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Numbers:',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      for (int digit in digits)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                digit.toString(),
+                                style: TextStyle(fontSize: 24, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Symbols:',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      for (String symbol in symbols)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.orange,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                symbol,
+                                style: TextStyle(fontSize: 24, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Formula:',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      for (int i = 0; i < 11; i++)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CustomDropdownButton(
+                            items: getDropdownItems(),
+                            value: selectedValues[i][0],
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedValues[i][0] = value;
+                                updateValue(i, value);
+                                print('Dropdown Item Index: $i, Value: $value');
+                              });
+                            },
+                          ),
+                        ),
+                    ],
+                  ),
+                  // New row for the label
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Card(
+                        color: resultLabel.contains('win')
+                            ? Colors.green
+                            : Colors.red, // Green for win, red for lose
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            resultLabel,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ]),
-
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  bool success = await startNewGame();
-                  setState(() {
-                    isGameStarted = success;
-                  });
+            Container(
+              width: 700, // Adjust the width as needed
+              child: FutureBuilder<void>(
+                future: _initializeControllerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return Transform.scale(
+                      scale: _controller.value.aspectRatio / MediaQuery.of(context).size.aspectRatio,
+                      child: Center(
+                        child: AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: CameraPreview(_controller),
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
                 },
-                child: Text('New Game'),
               ),
-              SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: isGameStarted ? () async {
-                  await calculateAndUpdateResult();
-                  setState(() {
-                    isGameStarted = false;
-                  });
-                } : null,
-                child: Text('Calculate'),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Numbers:',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(width: 10),
-              for (int digit in digits)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        digit.toString(),
-                        style: TextStyle(fontSize: 24, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Symbols:',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(width: 10),
-              for (String symbol in symbols)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        symbol,
-                        style: TextStyle(fontSize: 24, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Formula:',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(width: 10),
-              for (int i = 0; i < 11; i++)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CustomDropdownButton(
-                    items: getDropdownItems(),
-                    value: selectedValues[i][0],
-                    onChanged: (String? value) {
-                      setState(() {
-                        selectedValues[i][0] = value;
-                        updateValue(i, value);
-                        print('Dropdown Item Index: $i, Value: $value');
-                      });
-                    },
-                  ),
-                ),
-            ],
-          ),
-          // New row for the label
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Card(
-                color: resultLabel.contains('win')
-                    ? Colors.green
-                    : Colors.red, // Green for win, red for lose
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    resultLabel,
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        )
     );
   }
 
@@ -268,11 +326,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return items
         .map(
-          (item) => DropdownMenuItem<String?>(
+          (item) =>
+          DropdownMenuItem<String?>(
             value: item,
             child: Text(item),
           ),
-        )
+    )
         .toList();
   }
 
